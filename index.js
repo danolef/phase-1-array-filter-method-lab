84f010dacd1e4f7edc95510c005ec948eabdf048
Dan <dt.lefever@gmail.com>
2021-10-14 17:20:23 -0600

    commit tree 4a878c4a5cd231287ea397d9516f3fabe6c6bc06

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+
+ function findMatching(array, name) {
+    
+    function CbFun(element) {
+        return (element.toLowerCase() === name.toLowerCase())
+    }
+    
+    let returnArray= array.filter(CbFun)
+   
+    return returnArray
+}
+
+
+function fuzzyMatch (array, name){
+
+    function CbFun(element) {
+        return element.startsWith(name)
+    }
+
+    let returnArray= array.filter(CbFun)
+
+    return returnArray
+}
+
+function matchName (array, string) {
+
+    function nameFilter(element) {
+        return (element.name.toLowerCase() === string.toLowerCase())
+
+    }
+
+    return array.filter(nameFilter)
+}
